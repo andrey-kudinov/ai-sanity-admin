7c2e3a79b920cd6614de01517da42d8102038737
Andrey Kudinov <2499931@gmail.com>
2023-03-10 11:21:07 +0400

    Format a bit

--- schemas/client.ts
+++ schemas/client.ts
@@ -35,7 +35,7 @@ export default defineType({
         {
           title: 'Block',
           type: 'block',
-          styles: [{title: 'Normal', value: 'normal'}],
+          styles: [{ title: 'Normal', value: 'normal' }],
           lists: [],
         },
       ],
